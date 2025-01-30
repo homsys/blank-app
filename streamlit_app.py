8fe34b0bc74d1151a1d20571da70cb7ee0f84c08
Homsys <homsys@mail.ru>
2025-01-30 13:51:09 +1000

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,45 @@
-#  pip install streamlit
+# pip install streamlit
+# pip install requests
 
 import streamlit as st
+import requests
+
+list_crow = []
+params = {
+    'symbol': 'CROW',
+    'range': '1d',
+}
+
+
+def find_values(data, key):  # Функция для поиска значения
+    if isinstance(data, dict):
+        for k, v in data.items():
+            if k == key:
+                list_crow.append(v)
+            elif isinstance(v, (dict, list)):
+                find_values(v, key)
+    elif isinstance(data, list):
+        for item in data:
+            find_values(item, key)
 
 
 def main():
-    st.title("Тестируем телеграм приложение")
+    st.title("Курс CROW")
+    if st.button("Узнать"):
+        try:
+            response = requests.get('https://api.wemixplay.com/info/v2/price-chart', params=params)
+            response.raise_for_status()
+        except requests.exceptions.RequestException:
+            print("Ошибка get запроса получения цены, повторим через 5 минут")
 
-    # Здесь можно добавить элементы управления для взаимодействия с ботом
-    user_input = st.text_input("Введите сообщение для бота")
+        if response.ok:
+            list_crow.clear()
+            find_values(response.json(), 'p')
 
-    if st.button("Отправить"):
-        # Здесь можно добавить логику для отправки сообщения боту
-        st.write(f"Вы отправили: {user_input}")
+            average = sum(list_crow) / len(list_crow)  # Вычисляем среднее значение
+            average = round(average, 4)  # Округляем
+
+        st.write(f"Актуальный курс: {average}")
 
 
 if __name__ == '__main__':
