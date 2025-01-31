d6618f7d22c1912adf8f9c757b0be6e285a35172
homsys <56941346+homsys@users.noreply.github.com>
2025-01-31 18:05:24 +1000

    Update streamlit_app.py
    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ params = {
 TOKEN = st.secrets["discord"]["token"]
 
 # ID канала
-CHANNEL_ID = '941976229412761654'
+CHANNEL_ID = '941976229412761653'
 # URL для получения последнего сообщения
 url = f"https://discord.com/api/v10/channels/941976229412761653/messages"
 headers = {
