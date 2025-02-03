e8bc9098087cfad31c1831a5a43a5e5075301ca6
homsys <56941346+homsys@users.noreply.github.com>
2025-02-04 00:11:52 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,6 +176,12 @@ def main():
     # Вставляем CSS в Streamlit
     st.markdown(discord_style, unsafe_allow_html=True)
 
+    # Получаем параметры из URL
+    query_params = st.experimental_get_query_params()
 
+    # Выводим параметры
+    st.write("Query Parameters:", query_params)
+    
+    
 if __name__ == '__main__':
     main()
